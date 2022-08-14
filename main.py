22a51561948594e361f1615d5cfca25e74affeb9
Stefan Tesmanovic <tesmanovic166@gmail.com>
2022-08-14 16:38:05 +0200

    osnovni setup za pygame

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import pygame
 
+screenw = 500
+screenh = 500
+
+screen = pygame.display.set_mode((screenw, screenh))
+pygame.display.set_caption("Paint")
+
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            running = False
+    pygame.display.flip()
+pygame.quit()
